f4333134a720a538d206ef31e02e5e7f14c45fd5
JessicaSellers <jswx5@mail.missouri.edu>
2018-08-23 23:04:00 -0500

    Add files via upload

---
+++ Expenses/Expenses/Expenses.swift
@@ -0,0 +1,15 @@
+//
+//  Expenses.swift
+//  Expenses
+//
+//  Created by Jessica Elizabeth Sellers on 8/23/18.
+//  Copyright © 2018 Jessica Sellers. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
